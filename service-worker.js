962e2a9987bd688176f3f1fe2200f07b22904413
Daniel Schwartz <danielsch24@gmail.com>
2019-03-09 21:43:23 +0200

    fixed no toggle

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdf19e8ea5158a2220dc51675c6cf939.js"
+  "/precache-manifest.2be9d98f87dc4013aca20cfb7d2c07e7.js"
 );
 
 workbox.clientsClaim();

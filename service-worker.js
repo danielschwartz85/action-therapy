ebfaf11250d2e3a692a2d8163bf3910797ea6d29
Daniel Schwartz <danielsch24@gmail.com>
2020-08-04 13:25:03 +0300

    fix opnen pannel

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a198ff3c57b57f819f3c91f086f9304a.js"
+  "/precache-manifest.8b55727ac619839719aa65ff66633870.js"
 );
 
 workbox.clientsClaim();
